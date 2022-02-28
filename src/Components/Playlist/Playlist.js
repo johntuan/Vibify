e3f1028b550886098d53384ed563c68214ddbe80
John <john_ha80@hotmail.com>
2022-02-28 17:13:57 -0500

    Commit 2/28

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,9 +7,11 @@ import TrackList from '../TrackList/TrackList';
 class Playlist extends React.Component {
     render () {
         return (
-            <div class="Playlist">
+            <div className="Playlist">
                 <input defaultValue={"New Playlist"} />
-                <TrackList tracks = {this.props.playlistTracks} />
+                <TrackList tracks = {this.props.playlistTracks} 
+                            onRemove={this.props.onRemove}
+                            isRemoval={true}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
